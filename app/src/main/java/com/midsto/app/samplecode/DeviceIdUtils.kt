2729f1c6becf713c2ebd5e4117889960e54b0c11
cmunter <cmunter@gmail.com>
2019-12-11 16:56:28 +0100

    Use Random UUID for emulators

--- app/src/main/java/com/midsto/app/samplecode/DeviceIdUtils.kt
+++ app/src/main/java/com/midsto/app/samplecode/DeviceIdUtils.kt
@@ -1,17 +1,26 @@
 package com.midsto.app.samplecode
 
+import android.content.Context
 import android.provider.Settings.Secure
 import java.math.BigInteger
 import java.security.MessageDigest
+import java.util.*
 
 
 class DeviceIdUtils {
 
-    fun getDeviceId(): String? {
-        val androidUid = Secure.ANDROID_ID
+    fun getDeviceId(context: Context): String? {
+
+        // TODO Use Firebase instance ID
+        // implementation 'com.google.firebase:firebase-messaging:20.0.1'
+        // String reliableIdentifier = FirebaseInstanceId.getInstance().getId();
+
+        // https://en.wikipedia.org/wiki/Salt_(cryptography) Hashed value = SHA256 (Salt value + collector value)
+        // Use same salt value (fixed salt) for the same collector type, This will make it possible to match the same value without knowing the specific value
+
+        val androidUid = Secure.getString(context.contentResolver, Secure.ANDROID_ID)
         return if (androidUid.isEmpty()) {
-            // TODO generate random ID
-            "12356789" // for emulator testing
+            UUID.randomUUID().toString() // for emulator testing
         } else {
             var androidUidBytes = androidUid.toByteArray()
             val md5Digest = MessageDigest.getInstance("MD5")
